3da224467232a805fc6cd86fbf37f1b590ac4dc3
aiden30015 <loolloo080313@gmail.com>
2025-12-11 08:41:44 +0900

    💄:: Color 변경

--- lib/core/theme/color.dart
+++ lib/core/theme/color.dart
@@ -4,27 +4,27 @@ class WasherColor {
   WasherColor._();
 
   // main color
-  static const mainColor100 = Color(0xFF0059FF);
-  static const mainColor200 = Color(0xFF0E62FF);
-  static const mainColor400 = Color(0xFF1768FF);
-  static const mainColor500 = Color(0xFF2370FF);
+  static const mainColor100 = Color(0xFFDBE7FF);
+  static const mainColor200 = Color(0xFFB6D0FF);
+  static const mainColor400 = Color(0xFF85B0FF);
+  static const mainColor500 = Color(0xFF699DFF);
   static const mainColor600 = Color(0xFF4486FF);
-  static const mainColor700 = Color(0xFF699DFF);
-  static const mainColor800 = Color(0xFF85B0FF);
-  static const mainColor900 = Color(0xFFB6D0FF);
-  static const mainColor1000 = Color(0xFFDBE7FF);
+  static const mainColor700 = Color(0xFF2370FF);
+  static const mainColor800 = Color(0xFF1768FF);
+  static const mainColor900 = Color(0xFF0E62FF);
+  static const mainColor1000 = Color(0xFF0059FF);
 
   // base gray
   static const baseGray = Color(0xFF141414);
-  static const baseGray100 = Color(0xFF343434);
-  static const baseGray200 = Color(0xFF494949);
-  static const baseGray400 = Color(0xFF5F5F5F);
-  static const baseGray500 = Color(0xFF737373);
+  static const baseGray100 = Color(0xFFFBFBFB);
+  static const baseGray200 = Color(0xFFE6E6E6);
+  static const baseGray400 = Color(0xFFCACACA);
+  static const baseGray500 = Color(0xFFB0B0B0);
   static const baseGray600 = Color(0xFF969696);
-  static const baseGray700 = Color(0xFFB0B0B0);
-  static const baseGray800 = Color(0xFFCACACA);
-  static const baseGray900 = Color(0xFFE6E6E6);
-  static const baseGray1000 = Color(0xFFFBFBFB);
+  static const baseGray700 = Color(0xFF737373);
+  static const baseGray800 = Color(0xFF5F5F5F);
+  static const baseGray900 = Color(0xFF494949);
+  static const baseGray1000 = Color(0xFF343434);
 
   // error
   static const errorColor = Color(0xFFEA3B42);

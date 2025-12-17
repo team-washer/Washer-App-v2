819ad08a74da8b2b71fa909e2c49cdcf0780dabe
aiden30015 <loolloo080313@gmail.com>
2025-12-17 23:23:27 +0900

    💄 :: 세탁기 예약 상태 아이템 위젯 추가

---
+++ lib/presentation/home/widgets/landry_reservation_status_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:project_setting/core/enums/laundry_machine_type.dart';
+import 'package:project_setting/core/theme/color.dart';
+import 'package:project_setting/core/theme/typography.dart';
+
+class LandryReservationStatusItem extends StatelessWidget {
+  final LaundryMachineType machineType;
+  final int floor;
+  final int number;
+  final String side;
+  final bool isUsed;
+
+  const LandryReservationStatusItem({
+    super.key,
+    required this.machineType,
+    required this.floor,
+    required this.number,
+    required this.side,
+    required this.isUsed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String machine = machineType.text;
+
+    switch (machineType) {
+      case LaundryMachineType.washer:
+        machine = "Washer";
+        break;
+      case LaundryMachineType.dryer:
+        machine = "Dryer";
+        break;
+    }
+
+    return Container(
+      width: double.infinity, // 최대 너비 차지
+      padding: const EdgeInsets.symmetric(
+        horizontal: 17.5,
+        vertical: 12,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          machineType.icon(
+            color: isUsed ? WasherColor.baseGray200 : null,
+          ),
+          const SizedBox(width: 12),
+          Text(
+            '$machine-${floor}F-$side$number',
+            style: WasherTypography.body3(
+              isUsed ? WasherColor.baseGray200 : WasherColor.baseGray600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

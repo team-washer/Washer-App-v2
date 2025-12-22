dc01bf9343543c775136d68b091acd3065600d89
sysop <s24022@gsm.hs.kr>
2025-12-22 13:59:08 +0900

    ✨️ :: 라우트 설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_setting/firebase_options.dart';
 
+import 'core/router/app_router.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
     );
   }

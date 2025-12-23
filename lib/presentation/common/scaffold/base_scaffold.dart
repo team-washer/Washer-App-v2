3a34a7ec0af8b1f9c64edc530b8cae997e625c49
aiden30015 <loolloo080313@gmail.com>
2025-12-23 11:57:42 +0900

    ✨️ :: MainScaffold 클래스 추가 및 하단 네비게이션 바 통합

--- lib/presentation/common/scaffold/base_scaffold.dart
+++ lib/presentation/common/scaffold/base_scaffold.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:project_setting/core/theme/color.dart';
-import 'package:project_setting/presentation/common/scaffold/bottom_navigation_bar.dart';
 import 'package:project_setting/presentation/common/scaffold/washer_appbar.dart';
 
 class BaseScaffold extends StatelessWidget {
   final Widget body;
   final bool showAppBar;
-  final bool showBottomNav;
   final bool hasNotification;
-  final NavTabType? currentTab;
-  final ValueChanged<NavTabType>? onTabChanged;
+  final Widget? bottomNavigationBar;
 
   const BaseScaffold({
     super.key,
     required this.body,
     this.showAppBar = true,
-    this.showBottomNav = true,
     this.hasNotification = false,
-    this.currentTab,
-    this.onTabChanged,
-  }) : assert(
-         !showBottomNav || (currentTab != null && onTabChanged != null),
-         'showBottomNav이 true일 때 currentTab과 onTabChanged는 필수입니다.',
-       );
+    this.bottomNavigationBar,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +25,7 @@ class BaseScaffold extends StatelessWidget {
             ? WasherAppbar(hasNotification: hasNotification)
             : null,
         body: _buildBody(),
-        bottomNavigationBar: showBottomNav
-            ? WasherBottomNavigationBar(
-                currentTab: currentTab!,
-                onTabChanged: onTabChanged!,
-              )
-            : null,
+        bottomNavigationBar: bottomNavigationBar,
       ),
     );
   }
--- lib/presentation/common/scaffold/bottom_navigation_bar.dart
+++ lib/presentation/common/scaffold/bottom_navigation_bar.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_setting/core/theme/color.dart';
 import 'package:project_setting/core/theme/icon.dart';
-import 'package:project_setting/core/theme/spacing.dart';
-import 'package:project_setting/core/theme/typography.dart';
 
-/// 네비게이션 탭 아이템 정의
 enum NavTabType {
   dryer,
   home,
@@ -48,69 +45,24 @@ class WasherBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-      ),
-      child: SafeArea(
-        top: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: NavTabType.values.map((tab) {
-              return _NavTabItem(
-                tab: tab,
-                isSelected: currentTab == tab,
-                onTap: () => onTabChanged(tab),
-              );
-            }).toList(),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-/// 개별 탭 아이템 위젯
-class _NavTabItem extends StatelessWidget {
-  final NavTabType tab;
-  final bool isSelected;
-  final VoidCallback onTap;
-
-  const _NavTabItem({
-    required this.tab,
-    required this.isSelected,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final color = isSelected
-        ? WasherColor.baseGray400
-        : WasherColor.baseGray200;
-
-    return GestureDetector(
-      onTap: onTap,
-      behavior: HitTestBehavior.opaque,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          WasherIcon(
+    return BottomNavigationBar(
+      currentIndex: NavTabType.values.indexOf(currentTab),
+      onTap: (index) => onTabChanged(NavTabType.values[index]),
+      backgroundColor: Colors.white,
+      selectedItemColor: WasherColor.baseGray400,
+      unselectedItemColor: WasherColor.baseGray200,
+      type: BottomNavigationBarType.fixed,
+      items: NavTabType.values.map((tab) {
+        return BottomNavigationBarItem(
+          icon: WasherIcon(
             type: tab.iconType,
-            color: color,
+            color: currentTab == tab
+                ? WasherColor.baseGray400
+                : WasherColor.baseGray200,
           ),
-          const SizedBox(height: AppSpacing.v2),
-          Text(
-            tab.label,
-            style: WasherTypography.body2(color),
-          ),
-        ],
-      ),
+          label: tab.label,
+        );
+      }).toList(),
     );
   }
 }

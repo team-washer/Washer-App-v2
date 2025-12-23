ad15335cdbbd3c63eef2877b5fffe94018f12472
sysop <s24022@gsm.hs.kr>
2025-12-23 13:55:51 +0900

    ♻️ :: home -> main 으로 이름 변경

--- lib/presentation/main/screen/main_screen.dart
+++ lib/presentation/main/screen/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {

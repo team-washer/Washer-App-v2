17241e01c390a63ffeff0b5ea1eb4bbe8d20a0a8
sysop <s24022@gsm.hs.kr>
2025-12-23 19:50:17 +0900

    ✨ :: 라우트 패스 추가

--- lib/core/router/route_paths.dart
+++ lib/core/router/route_paths.dart
@@ -1,3 +1,11 @@
 class RoutePaths {
-  static const main = '/';
+  static const main = '/main';
+
+  // 탭 라우트
+  static const dryer = '/dryer';
+  static const home = '/home';
+  static const washer = '/washer';
+
+  // 탭 밖 라우트(예: 알림)
+  static const alarm = '/alarm';
 }

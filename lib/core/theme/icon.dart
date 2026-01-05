e53a53a92c41c48f8ffc6b11c01210e8befa10fa
sysop <s24022@gsm.hs.kr>
2026-01-05 17:10:10 +0900

    ♻️ :: 아이콘과 아이콘버튼 분리 및 가독성 개선

--- lib/core/theme/icon.dart
+++ lib/core/theme/icon.dart
@@ -3,58 +3,69 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 enum WasherIconType {
   back,
+  home,
+  map,
+  history,
   cancel,
   down,
-  dry,
+  notification,
   eye,
   eyeOff,
-  history,
-  home,
+  dry,
+  water,
+  user,
   logo,
   logoWithTitle,
-  map,
-  notification,
   triangleWarning,
-  user,
-  water,
+  warningCircle,
   dryCircle,
   waterCircle,
-  warningCircle,
   historyCircle,
 }
 
+/// [WasherIconType]을 SVG 파일명으로 변환하는 확장
 extension WasherIconTypeExtension on WasherIconType {
+  /// SVG 파일명 반환
   String get assetName {
     const assetMap = {
       WasherIconType.back: 'back.svg',
+      WasherIconType.home: 'home.svg',
+      WasherIconType.map: 'map.svg',
+      WasherIconType.history: 'history.svg',
       WasherIconType.cancel: 'cancel.svg',
       WasherIconType.down: 'down.svg',
-      WasherIconType.dry: 'dry.svg',
+      WasherIconType.notification: 'notification.svg',
       WasherIconType.eye: 'eye.svg',
       WasherIconType.eyeOff: 'eye_off.svg',
-      WasherIconType.history: 'history.svg',
-      WasherIconType.home: 'home.svg',
+      WasherIconType.dry: 'dry.svg',
+      WasherIconType.water: 'water.svg',
+      WasherIconType.user: 'user.svg',
       WasherIconType.logo: 'logo.svg',
       WasherIconType.logoWithTitle: 'logo_with_title.svg',
-      WasherIconType.map: 'map.svg',
-      WasherIconType.notification: 'notification.svg',
       WasherIconType.triangleWarning: 'triangle_warning.svg',
-      WasherIconType.user: 'user.svg',
-      WasherIconType.water: 'water.svg',
+      WasherIconType.warningCircle: 'warning_circle.svg',
       WasherIconType.dryCircle: 'dry_circle.svg',
       WasherIconType.waterCircle: 'water_circle.svg',
-      WasherIconType.warningCircle: 'warning_circle.svg',
       WasherIconType.historyCircle: 'history_circle.svg',
     };
     return assetMap[this] ?? 'default_icon.svg';
   }
+
+  /// assets 폴더의 전체 경로 반환
+  String get assetPath => 'assets/icons/$assetName';
 }
 
-/// 디자인 시스템 공용 아이콘 위젯
 class WasherIcon extends StatelessWidget {
+  /// 아이콘 타입
   final WasherIconType type;
+
+  /// 아이콘 크기 (기본값: 24.0)
   final double size;
+
+  /// 아이콘 색상 (null일 경우 원본 색상 사용)
   final Color? color;
+
+  /// 탭 이벤트 핸들러
   final VoidCallback? onTap;
 
   const WasherIcon({
@@ -68,16 +79,63 @@ class WasherIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final iconWidget = SvgPicture.asset(
-      'assets/icons/${type.assetName}',
+      type.assetPath,
       width: size,
       height: size,
-      colorFilter: color != null
-          ? ColorFilter.mode(color!, BlendMode.srcIn)
-          : null,
+      colorFilter: _buildColorFilter(),
     );
 
-    return onTap != null
-        ? GestureDetector(onTap: onTap, child: iconWidget)
-        : iconWidget;
+    // 탭 이벤트가 있으면 GestureDetector로 감싸기
+    if (onTap != null) {
+      return GestureDetector(
+        onTap: onTap,
+        child: iconWidget,
+      );
+    }
+
+    return iconWidget;
+  }
+
+  /// 색상 필터 생성
+  /// color가 null이면 null 반환 (원본 색상 사용)
+  ColorFilter? _buildColorFilter() {
+    return color != null ? ColorFilter.mode(color!, BlendMode.srcIn) : null;
+  }
+}
+
+/// 터치 가능한 아이콘 버튼 위젯
+class WasherIconButton extends StatelessWidget {
+  final WasherIconType type;
+  final double size;
+  final Color? color;
+  final VoidCallback? onTap;
+  final EdgeInsetsGeometry padding;
+
+  const WasherIconButton({
+    super.key,
+    required this.type,
+    this.size = 24.0,
+    this.color,
+    this.onTap,
+    this.padding = const EdgeInsets.all(8.0),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(size / 2 + 8),
+        child: Padding(
+          padding: padding,
+          child: WasherIcon(
+            type: type,
+            size: size,
+            color: color,
+          ),
+        ),
+      ),
+    );
   }
 }

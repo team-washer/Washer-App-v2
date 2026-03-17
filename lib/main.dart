824818d7419ad9ed202b5b2a24a59d032e5ea3ea
aiden30015 <loolloo080313@gmail.com>
2026-03-17 11:04:36 +0900

    ✨ :: 알림 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:washer/core/notifications/notification_bootstrapper.dart';
 import 'package:washer/firebase_options.dart';
 
 import 'core/router/app_router.dart';
@@ -9,14 +10,16 @@ import 'core/router/app_router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 환경 변수 로드
   await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: NotificationBootstrapper(
+        child: MyApp(),
+      ),
     ),
   );
 }
@@ -33,3 +36,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
